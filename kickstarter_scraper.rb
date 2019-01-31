3454240d9312ccc3bd1845cf9e343ab51835d8d9
Stephen Wright <scw248@gmail.com>
2019-01-31 16:03:17 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -3,7 +3,7 @@ require "nokogiri"
 require "pry"
 
 def create_project_hash
-  File.read('fixtures/kickstarter.html')
+  html = File.read('fixtures/kickstarter.html')
   
   kickstarter = Nokogiri::HTML(html)
   binding.pry
