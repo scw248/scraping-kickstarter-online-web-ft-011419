666b4abb942f550795e09ecc7fee731514a129a4
Stephen Wright <scw248@gmail.com>
2019-01-31 16:36:37 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -20,3 +20,5 @@ create_project_hash
 # description: project.css("p.bbcard_blurb").text
 
 # location: project.css("ul.project-meta span.location-name").text
+
+## percent_funded: project.css("ul.project-stats li.first.funded strong").text.gsub("%","").to_i
